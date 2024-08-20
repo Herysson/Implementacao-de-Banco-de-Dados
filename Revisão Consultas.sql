dee4c788166ce0022931b7478b3b68b21ad86a98
Herysson Rodrigues Figueiredo <heryssonf@gmail.com>
2024-08-20 14:31:48 -0300

    Update Revisão Consultas.sql

--- Revisão Consultas.sql
+++ Revisão Consultas.sql
@@ -149,3 +149,83 @@ JOIN TRABALHA_EM AS T2
 	AND T1.Horas = T2.Horas
 WHERE T2.Fcpf = '33344555587';
 
+-- JOIN
+INSERT INTO FUNCIONARIO (Pnome, Minicial, Unome, Cpf, Datanasc, Endereco, Sexo, Salario, Cpf_supervisor, Dnr)
+VALUES ('Carlos', 'M', 'Ferreira', '12312312311', '1980-02-15', 'Av. Paulista, 1000, São Paulo, SP', 'M', 45000, NULL, NULL);
+
+INSERT INTO FUNCIONARIO (Pnome, Minicial, Unome, Cpf, Datanasc, Endereco, Sexo, Salario, Cpf_supervisor, Dnr)
+VALUES ('Mariana', 'L', 'Gomes', '32132132122', '1985-06-22', 'Rua das Acácias, 500, Rio de Janeiro, RJ', 'F', 42000, NULL, NULL);
+
+INSERT INTO FUNCIONARIO (Pnome, Minicial, Unome, Cpf, Datanasc, Endereco, Sexo, Salario, Cpf_supervisor, Dnr)
+VALUES ('Pedro', 'A', 'Silva', '65465465433', '1990-11-10', 'Rua da Praia, 200, Salvador, BA', 'M', 47000, NULL, NULL);
+
+INSERT INTO DEPARTAMENTO (Dnome, Dnumero)
+VALUES ('Vendas', 6);
+
+INSERT INTO DEPARTAMENTO (Dnome, Dnumero)
+VALUES ('Recursos Humanos', 7);
+
+INSERT INTO DEPARTAMENTO (Dnome, Dnumero)
+VALUES ('TI', 8);
+
+--LEFT JOIN 
+SELECT 
+    FUNCIONARIO.Pnome,
+    FUNCIONARIO.Minicial,
+    FUNCIONARIO.Unome,
+    FUNCIONARIO.Cpf,
+    FUNCIONARIO.Datanasc,
+    FUNCIONARIO.Endereco,
+    FUNCIONARIO.Sexo,
+    FUNCIONARIO.Salario,
+    FUNCIONARIO.Cpf_supervisor,
+    FUNCIONARIO.Dnr,
+    DEPARTAMENTO.Dnome
+FROM 
+    FUNCIONARIO
+LEFT JOIN 
+    DEPARTAMENTO 
+ON 
+    FUNCIONARIO.Dnr = DEPARTAMENTO.Dnumero;
+
+--RIGHT JOIN
+SELECT 
+    FUNCIONARIO.Pnome,
+    FUNCIONARIO.Minicial,
+    FUNCIONARIO.Unome,
+    FUNCIONARIO.Cpf,
+    FUNCIONARIO.Datanasc,
+    FUNCIONARIO.Endereco,
+    FUNCIONARIO.Sexo,
+    FUNCIONARIO.Salario,
+    FUNCIONARIO.Cpf_supervisor,
+    FUNCIONARIO.Dnr,
+    DEPARTAMENTO.Dnome
+FROM 
+    FUNCIONARIO
+RIGHT JOIN 
+    DEPARTAMENTO 
+ON 
+    FUNCIONARIO.Dnr = DEPARTAMENTO.Dnumero;
+
+--FULL JOIN
+SELECT 
+    FUNCIONARIO.Pnome,
+    FUNCIONARIO.Minicial,
+    FUNCIONARIO.Unome,
+    FUNCIONARIO.Cpf,
+    FUNCIONARIO.Datanasc,
+    FUNCIONARIO.Endereco,
+    FUNCIONARIO.Sexo,
+    FUNCIONARIO.Salario,
+    FUNCIONARIO.Cpf_supervisor,
+    FUNCIONARIO.Dnr,
+    DEPARTAMENTO.Dnome
+FROM 
+    FUNCIONARIO
+FULL JOIN 
+    DEPARTAMENTO 
+ON 
+    FUNCIONARIO.Dnr = DEPARTAMENTO.Dnumero;
+
+
