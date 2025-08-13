61f6a969a856517631d82d1991bd1de029dbc55a
Herysson Rodrigues Figueiredo <heryssonf@gmail.com>
2025-08-12 21:39:31 -0300

    Update Aula 03 - Consultas Joins.sql

--- Aula 03 - Consultas Joins.sql
+++ Aula 03 - Consultas Joins.sql
@@ -21,26 +21,15 @@ FUNCIONARIO ON
 Cpf_gerente =Cpf)
 WHERE Projlocal=‘Mauá’;
 
-
-
-
 INSERT INTO FUNCIONARIO (Pnome, Minicial, Unome, Cpf, Datanasc, Endereco, Sexo, Salario, Cpf_supervisor, Dnr)
-VALUES ('Carlos', 'M', 'Ferreira', '12312312311', '1980-02-15', 'Av. Paulista, 1000, São Paulo, SP', 'M', 45000, NULL, NULL);
-
-INSERT INTO FUNCIONARIO (Pnome, Minicial, Unome, Cpf, Datanasc, Endereco, Sexo, Salario, Cpf_supervisor, Dnr)
-VALUES ('Mariana', 'L', 'Gomes', '32132132122', '1985-06-22', 'Rua das Acácias, 500, Rio de Janeiro, RJ', 'F', 42000, NULL, NULL);
-
-INSERT INTO FUNCIONARIO (Pnome, Minicial, Unome, Cpf, Datanasc, Endereco, Sexo, Salario, Cpf_supervisor, Dnr)
-VALUES ('Pedro', 'A', 'Silva', '65465465433', '1990-11-10', 'Rua da Praia, 200, Salvador, BA', 'M', 47000, NULL, NULL);
+VALUES ('Carlos', 'M', 'Ferreira', '12312312311', '1980-02-15', 'Av. Paulista, 1000, São Paulo, SP', 'M', 45000, NULL, NULL),
+('Mariana', 'L', 'Gomes', '32132132122', '1985-06-22', 'Rua das Acácias, 500, Rio de Janeiro, RJ', 'F', 42000, NULL, NULL),
+('Pedro', 'A', 'Silva', '65465465433', '1990-11-10', 'Rua da Praia, 200, Salvador, BA', 'M', 47000, NULL, NULL);
 
 INSERT INTO DEPARTAMENTO (Dnome, Dnumero)
-VALUES ('Vendas', 6);
-
-INSERT INTO DEPARTAMENTO (Dnome, Dnumero)
-VALUES ('Recursos Humanos', 7);
-
-INSERT INTO DEPARTAMENTO (Dnome, Dnumero)
-VALUES ('TI', 8);
+VALUES ('Vendas', 6),
+('RH', 7),
+('TI', 8);
 
 --LEFT JOIN 
 -- Econtre os funcionarios que não possuem um departamento a eles vinculado
@@ -324,5 +313,6 @@ HAVING
         WHERE PROJETO.Projlocal = 'São Paulo'
         GROUP BY PROJETO.Projnumero
     );
+
 
 
